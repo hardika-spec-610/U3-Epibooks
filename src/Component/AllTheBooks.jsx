63e0b4c745bfe499366295f32e6686348d8087df
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-01-31 18:03:06 +0100

    implement category button functionality

--- src/Component/AllTheBooks.jsx
+++ src/Component/AllTheBooks.jsx
@@ -1,17 +1,29 @@
 import { Component } from "react";
-import { Card, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
-import items from "../books/history.json";
+import { Button, Card, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import fantasy from "../books/fantasy.json";
+import history from "../books/history.json";
+import horror from "../books/horror.json";
+import romance from "../books/romance.json";
+import scifi from "../books/scifi.json";
 import './componentStyles.css'
 
 class AllTheBooks extends Component{
+    state={
+        selectedBookCategory: fantasy
+    }
     render() {
         return (
             <Container>
+                <Button variant="success" className="mr-3 mb-5" onClick={()=>  {this.setState({selectedBookCategory:fantasy})}}>Fantasy</Button>
+                <Button variant="success" className="mr-3 mb-5" onClick={() => {this.setState({selectedBookCategory:history})}}>History</Button>
+                <Button variant="success" className="mr-3 mb-5" onClick={() => {this.setState({selectedBookCategory:horror})}}>Horror</Button>
+                <Button variant="success" className="mr-3 mb-5" onClick={() => {this.setState({selectedBookCategory:romance})}}>Romance</Button>
+                <Button variant="success" className="mr-3 mb-5" onClick={() => {this.setState({selectedBookCategory:scifi})}}>Scifi</Button>
                 <Row>
-                    {items.map((singleBook) =>{
+                    {this.state.selectedBookCategory.map((singleBook) =>{
                         return(
-                            <Col xs={12} sm={12} md={6} lg={3} className="d-flex mb-lg-4 mb-md-4 mb-sm-3 mb-xs-3" >
-                                <Card className="w-100">
+                            <Col  key={singleBook.asin}  xs={12} sm={12} md={6} lg={3} className="d-flex mb-lg-4 mb-md-4 mb-sm-3 mb-xs-3" >
+                                <Card className="w-100 shadow-sm">
                                     <Card.Img variant="top" src={singleBook.img} alt={singleBook.title} height="300px" />
                                     <Card.Body>
                                       <Card.Title className="text-truncate">{singleBook.title}</Card.Title>
