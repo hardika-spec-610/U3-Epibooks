b3944253146817eb84b5151726147dece22b96ab
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-02-01 18:03:02 +0100

    search functionality

--- src/Component/BookList.jsx
+++ src/Component/BookList.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import fantasy from "../books/fantasy.json";
 import history from "../books/history.json";
 import horror from "../books/horror.json";
@@ -11,71 +11,118 @@ import "./componentStyles.css";
 class BookList extends Component {
   state = {
     selectedBookCategory: fantasy,
+    searchQuery: "",
   };
   render() {
     return (
       <Container>
-        <Button
-          variant="success"
-          className="mr-3 mb-5"
-          onClick={() => {
-            this.setState({ selectedBookCategory: fantasy });
-          }}
-        >
-          Fantasy
-        </Button>
-        <Button
-          variant="success"
-          className="mr-3 mb-5"
-          onClick={() => {
-            this.setState({ selectedBookCategory: history });
-          }}
-        >
-          History
-        </Button>
-        <Button
-          variant="success"
-          className="mr-3 mb-5"
-          onClick={() => {
-            this.setState({ selectedBookCategory: horror });
-          }}
-        >
-          Horror
-        </Button>
-        <Button
-          variant="success"
-          className="mr-3 mb-5"
-          onClick={() => {
-            this.setState({ selectedBookCategory: romance });
-          }}
-        >
-          Romance
-        </Button>
-        <Button
-          variant="success"
-          className="mr-3 mb-5"
-          onClick={() => {
-            this.setState({ selectedBookCategory: scifi });
-          }}
-        >
-          Scifi
-        </Button>
-        <Row>
-          {this.state.selectedBookCategory.map((currentBook) => {
-            return (
-              <Col
-                key={currentBook.asin}
-                xs={12}
-                sm={12}
-                md={6}
-                lg={3}
-                className="d-flex mb-lg-4 mb-md-4 mb-sm-3 mb-xs-3"
-              >
-                <SingleBook singleBook={currentBook} />
-              </Col>
-            );
-          })}
-        </Row>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Search your book here"
+            value={this.state.searchQuery}
+            onChange={(e) => {
+              console.log("letter inputted!", e.target.value);
+              this.setState({ searchQuery: e.target.value });
+            }}
+          />
+        </Form.Group>
+        {this.state.searchQuery ? (
+          <div>
+            {this.state.searchQuery.length > 2 && (
+              <div>
+                <h4>Searched Books</h4>
+                <Row>
+                  {this.state.selectedBookCategory
+                    .filter((selectedBook) => {
+                      // console.log(selectedBook);
+                      return selectedBook.title
+                        .toLowerCase()
+                        .includes(this.state.searchQuery);
+                    })
+                    .map((selectedBook) => (
+                      <Col
+                        xs={12}
+                        sm={12}
+                        md={4}
+                        lg={3}
+                        className="d-flex mb-lg-4 mb-md-4 mb-sm-3 mb-xs-3"
+                        key={selectedBook.asin}
+                      >
+                        <SingleBook singleBook={selectedBook} />
+                      </Col>
+                    ))}
+                </Row>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>
+            <Button
+              variant="success"
+              className="mr-3 mb-5"
+              onClick={() => {
+                this.setState({ selectedBookCategory: fantasy });
+              }}
+            >
+              Fantasy
+            </Button>
+            <Button
+              variant="success"
+              className="mr-3 mb-5"
+              onClick={() => {
+                this.setState({ selectedBookCategory: history });
+              }}
+            >
+              History
+            </Button>
+            <Button
+              variant="success"
+              className="mr-3 mb-5"
+              onClick={() => {
+                this.setState({ selectedBookCategory: horror });
+              }}
+            >
+              Horror
+            </Button>
+            <Button
+              variant="success"
+              className="mr-3 mb-5"
+              onClick={() => {
+                this.setState({ selectedBookCategory: romance });
+              }}
+            >
+              Romance
+            </Button>
+            <Button
+              variant="success"
+              className="mr-3 mb-5"
+              onClick={() => {
+                this.setState({ selectedBookCategory: scifi });
+              }}
+            >
+              Scifi
+            </Button>
+            <Row>
+              {this.state.selectedBookCategory
+                .slice(0, 12)
+                .map((currentBook) => {
+                  return (
+                    <Col
+                      key={currentBook.asin}
+                      xs={12}
+                      sm={12}
+                      md={4}
+                      lg={3}
+                      className="d-flex mb-lg-4 mb-md-4 mb-sm-3 mb-xs-3"
+                    >
+                      <SingleBook singleBook={currentBook} />
+                    </Col>
+                  );
+                })}
+            </Row>
+          </div>
+        )}
       </Container>
     );
   }
--- src/Component/Welcome.jsx
+++ src/Component/Welcome.jsx
@@ -2,20 +2,19 @@ import { Component } from "react";
 import { Container, Jumbotron } from "react-bootstrap";
 
 class Welcome extends Component {
-render() {
+  render() {
     return (
-        <Jumbotron fluid>
+      <Jumbotron fluid>
         <Container>
-          <h1>Over 10,000 Free and Premium 
-            Books In Your Pocket!</h1>
+          <h1>Over 10,000 Free and Premium Books In Your Pocket!</h1>
           <p className="hero-text">
-          TreeBooks is Africa foremost online book Store where 
-        Authors can make their Books available as ebook or 
-        audio Books to reach Millions of Buyers worldwide
+            TreeBooks is Africa foremost online book Store where Authors can
+            make their Books available as ebook or audio Books to reach Millions
+            of Buyers worldwide
           </p>
         </Container>
-      </Jumbotron> 
-    )
+      </Jumbotron>
+    );
+  }
 }
-}
-export default Welcome
+export default Welcome;
